3d50b6a2a450d23accd5b718aff2710b87ad1bcb
Johnny <johnny.peixotto@outlook.com>
2020-01-07 18:31:13 -0300

    building gobarber api

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'f9d7722b3a8eba1dcef23bc5588360a0', // gobarber@node2
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+// configuration database file
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
